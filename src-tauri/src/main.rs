65656609b923e3581347dee85e23c02bc2805404
Xurookie123 <123869189+Xurookie123@users.noreply.github.com>
2025-03-10 23:11:16 +0800

    update main rust from pakeplus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Menu, MenuItem, Submenu};
+
 fn main() {
-    pakeplus_lib::run()
+    let edit_menu = Submenu::new(
+        "Edit",
+        Menu::new()
+            .add_native_item(MenuItem::Undo)
+            .add_native_item(MenuItem::Redo)
+            .add_native_item(MenuItem::Copy)
+            .add_native_item(MenuItem::Cut)
+            .add_native_item(MenuItem::Paste)
+            .add_native_item(MenuItem::SelectAll)
+            .add_native_item(MenuItem::CloseWindow)
+            .add_native_item(MenuItem::Quit),
+    );
+    tauri::Builder::default()
+        .setup(|app| {
+            let _window = tauri::WindowBuilder::new(
+                app,
+                "PakePlus",
+                tauri::WindowUrl::App("http://www.xiaohongshu.com/explore".into()),
+            )
+            .initialization_script(include_str!("./extension/custom.js"))
+            .title("小红书")
+            .inner_size(1024.0, 768.0)
+            .center()
+            .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15")
+            .build()?;
+            Ok(())
+        })
+        .menu(Menu::new().add_submenu(edit_menu))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
